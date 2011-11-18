23aedea9da708e991dcd93cea871a93ab0eecdf1
moliware <miguel@moliware.com>
2011-11-18 18:22:20 +0100

    Removed some extra information

--- setup.py
+++ setup.py
@@ -9,7 +9,5 @@ setup(name = 'scraper',
       author = scraper.__author__,
       author_email = scraper.__email__,
       url = 'https://github.com/fjavieralba/scraper',
-      data_files = [('/etc/scraper', ['conf/scraper.yml'])],
-      packages = ['scraper', 'scraper.sites'],
-      scripts = ['scripts/run_scraper.py']
+      packages = ['scraper'],
       )
