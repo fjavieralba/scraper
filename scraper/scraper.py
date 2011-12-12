458bb1d910dd0d7dfad3992149f6f199b0c72a5c
fjavieralba <fjavieralba@gmail.com>
2011-12-12 23:46:38 +0100

    bug fixes

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -23,11 +23,12 @@ def scrapes(html_string, conf):
 def process(html_tree, html_string, conf):
     result = {}
     for field in conf:
+        #extract field content with xpath or regexp:
+        scraped = None
         if 'xpath' in conf[field]:
             xpath = conf[field]['xpath']
             scraped = html_tree.xpath(xpath)
-        
-            if isinstance(scraped, list):
+            if isinstance(scraped, list): #element list
                 try:
                     scraped = map(lambda x : x.text, scraped)
                 except:
@@ -35,38 +36,44 @@ def process(html_tree, html_string, conf):
         elif 'regexp' in conf[field]:
             regexp = conf[field]['regexp']
             scraped = re.findall(regexp, html_string)
-
-        #apply transformations (if defined)
-        value = scraped
-        if 'transf' in conf[field]:
-            #apply transformations in chain:
-            for func in conf[field]['transf']:
-                if isinstance(value, list): #list value
-                    try:
-                        value = map(func, scraped)
-                    except:
-                        break #dont include erroneous field
-                else: #single value:
-                    try:
-                        value = func(value)
-                    except Exception as e:
-                        print "Error applying func %s to field value %s" % (func, value)
-                        value = None
-                        break #dont include erroneous field
-            if value is not None: result[field] = value
-            else: #we got None as a result of applying transformations
-                if 'default' in conf[field]:
-                    scraped = conf[field]['default']
         
-        #encode field if character encoding is defined:
+        if scraped is not None:
+            #encode field if character encoding is defined:
             if 'encoding' in conf[field]:
                 encoding = conf[field]['encoding']
                 if encoding is not None:
-                    try:
-                        scraped = scraped.decode(encoding)
-                    except Exception as e:
-                        print "Error decoding %s field: %s" % (field, e)
-        
+                    if isinstance(scraped, list): #list value
+                        try:
+                            scraped = map(lambda x : x.decode(encoding), scraped)
+                        except Exception as e:
+                            print "Error decoding %s field: %s" % (field, e)
+                    else: #single value
+                        try:
+                            scraped = scraped.decode(encoding)
+                        except Exception as e:
+                            print "Error decoding %s field: %s" % (field, e)
+    
+            #apply transformations (if defined)
+            if 'transf' in conf[field]:
+                #apply transformations in chain:
+                for func in conf[field]['transf']:
+                    if isinstance(scraped, list): #list value
+                        try:
+                            scraped = map(func, scraped)
+                        except Exception as e:
+                            print "Error applying function %s to element list: %s" % (func, e)
+                            scraped = None
+                            break #dont include erroneous field
+                    else: #single value:
+                        try:
+                            scraped = func(scraped)
+                        except Exception as e:
+                            print "Error applying func %s to field value %s" % (func, scraped)
+                            scraped = None
+                            break #dont include erroneous field
+                if scraped is None: #some error occurred as a result of applying transformations
+                    if 'default' in conf[field]:
+                        scraped = conf[field]['default']
         result[field] = scraped
 
     return result	
